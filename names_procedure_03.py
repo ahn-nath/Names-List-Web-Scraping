3178ba9685eaf1a10490b4de93a3ec2e9b7186bb
ahn-nath <67247850+ahn-nath@users.noreply.github.com>
2021-03-14 18:47:49 -0400

    Initial commit II

---
+++ names_procedure_03.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import webdrivermanager
+
+
+# 1. setup 
+# **REMEMBER** remove path to your local folder
+driver = webdriver.Chrome("C:/Users/Ahn/AppData/Local/salabs_/WebDriverManager/bin/chromedriver.exe")
+dict_female = dict()
+dict_male = dict()
